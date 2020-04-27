1e49887c0db29a285147610a01baf49668cb5eca
Pavel I <ipa0486@gmail.com>
2020-04-27 18:47:49 +0300

    update for production

--- Gemfile
+++ Gemfile
@@ -31,6 +31,10 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'react-rails'
 
+group :production do
+  gem 'rails_12factor'
+end
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,16 +1,16 @@
 class Task < ApplicationRecord
   belongs_to :project
-  Project.reload
+  # Project.Reload
   validates :priority,
             numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :name, presence: true
   validates :project, presence: true
 
-  validates :project_id,
-            inclusion: {
-              in: Project.ids,
-              message:
-                'id %{value} is not valid. It should be one of ' +
-                  Project.ids.join(', ')
-            }
+  # validates :project_id,
+  #           inclusion: {
+  #             in: Project.ids,
+  #             message:
+  #               'id %{value} is not valid. It should be one of ' +
+  #                 Project.ids.join(', ')
+  #           }
 end
