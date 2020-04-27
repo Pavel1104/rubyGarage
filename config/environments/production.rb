c95515c7ebc975f8ce2818333ef1e54c7a4dc71a
Pavel I <ipa0486@gmail.com>
2020-04-27 20:32:12 +0300

    fimd solution 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,10 +30,13 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :scss
+
+  # config.assets.precompile += %w[ bootstrap/*.css bootstrap/*.js ]
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
